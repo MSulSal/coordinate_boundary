7a4a14d0d27c8cdb00800d0fec941e178da37e05
MSulSal <msulsaleem@gmail.com>
2024-03-27 18:48:09 -0700

    fix figure

--- boundaries.py
+++ boundaries.py
@@ -15,10 +15,10 @@ figure, axes = plt.subplots()
 plt.scatter(points_x, points_y)
 
 #plot boundaries created by minimum and maximum x and y values
-plt.plot([min_x, min_y], [-6000, 6000])
-plt.plot([max_x, max_x], [-6000, 6000])
-plt.plot([-6000, 6000], [min_y, min_y])
-plt.plot([-6000, 6000], [max_y, max_y])
+plt.plot([min_x, min_y], [-2000, 2000])
+plt.plot([max_x, max_x], [-2000, 2000])
+plt.plot([-2000, 2000], [min_y, min_y])
+plt.plot([-2000, 2000], [max_y, max_y])
 plt.plot([min_x, max_x], [min_y, max_y])
 
 
@@ -26,8 +26,8 @@ plt.plot([min_x, max_x], [min_y, max_y])
 radius = ((((max_x - min_x) ** 2) + ((max_y - min_y) ** 2)) ** 0.5) / 2
 tup = (((max_x + min_x) / 2), ((max_y + min_y) / 2))
 
-axes.set_xlim([-6000, 6000])
-axes.set_ylim([-6000, 6000])
+axes.set_xlim([-2000, 2000])
+axes.set_ylim([-2000, 2000])
 axes.add_artist(plt.Circle(tup, radius, facecolor="none", edgecolor="black"))
 
 plt.show()
